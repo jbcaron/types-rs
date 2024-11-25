7e229e2f461444fedbf6a8da8e14752c4dc01eb7
jbcaron <jbcaron.dev@gmail.com>
2024-11-25 19:27:54 +0100

    fix: change gas vector to u128

--- crates/starknet-types-rpc/src/v0_7_1/starknet_api_openrpc.rs
+++ crates/starknet-types-rpc/src/v0_7_1/starknet_api_openrpc.rs
@@ -595,9 +595,9 @@ pub struct ExecutionResources {
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub struct DataAvailability {
     /// the data gas consumed by this transaction's data, 0 if it uses gas for DA
-    pub l1_data_gas: u64,
+    pub l1_data_gas: u128,
     /// the gas consumed by this transaction's data, 0 if it uses data gas for DA
-    pub l1_gas: u64,
+    pub l1_gas: u128,
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
